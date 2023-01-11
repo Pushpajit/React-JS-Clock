a068054d95e8a3b646c9071bece656b5f0a05b1f
Pushpajit Biswas <pushpajitnexus007@gmail.com>
2023-01-12 00:18:03 +0530

    emoji and conditional rendering with header added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React from 'react';
 import './App.css';
 import Dial from './components/Dial';
+import Header from './components/Header';
 
 function App() {
+  const [date, setDate] = React.useState({hour:0, min:0, sec:0, day:0, date:0, month:0})
+
+    React.useEffect(() => {
+
+        setInterval(() => {
+          setDate((prev) => {
+            const time = new Date()
+              return {
+                  hour: time.getHours(),
+                  min: time.getMinutes(),
+                  sec: time.getSeconds(),
+                  day: time.getDay(),
+                  date: time.getDate(),
+                  month: time.getMonth()
+                  
+              }
+          })
+        }, 1000)
+
+    },[])
+
+
   return (
     <div className='container'>
-      <Dial />
+      <Header date= {date}/>
+      {/* Sending date state as a props */}
+      <Dial date= {date}/>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
-function Header() {
+function Header({date}) {
   return (
-    <div>
-      
-    </div>
+    <header>
+      {(date.hour >= 0 && date.hour < 4) && "Good Night 😪"}
+      {(date.hour >= 4 && date.hour < 12) && "Good Morning 🌄🌞✨"}
+      {(date.hour >= 12 && date.hour < 18) && "Good Afternoon 🌅🎇🎉"}
+      {(date.hour >= 18 && date.hour < 24) && "Good Evening 🌙🎇"}
+    </header>
   )
 }
 
