4fc6b496d1012bdd714d6e6da3cd94e462b13e9d
Pushpajit Biswas <pushpajitnexus007@gmail.com>
2023-01-12 21:15:12 +0530

    weather api added, need to modify

--- src/App.js
+++ src/App.js
@@ -4,14 +4,18 @@ import Dial from './components/Dial';
 import Header from './components/Header';
 
 function App() {
-  const [date, setDate] = React.useState({hour:0, min:0, sec:0, day:0, date:0, month:0})
+  const [date, setDate] = React.useState({hour:0, min:0, sec:0, day:0, date:0, month:0, temp: 14})
+  
+  // fetch(API).then(res => res.json()).then(data => console.log(data))
 
-    React.useEffect(() => {
+
+  React.useEffect(() => {
 
         setInterval(() => {
           setDate((prev) => {
             const time = new Date()
               return {
+                  ...prev,
                   hour: time.getHours(),
                   min: time.getMinutes(),
                   sec: time.getSeconds(),
@@ -23,7 +27,21 @@ function App() {
           })
         }, 1000)
 
+        
     },[])
+
+  React.useEffect(()=> {
+    setInterval(() => {
+        console.log("Fetch Ran")
+        setDate(prev => {
+          return{
+            ...prev,
+            temp: prev.temp + 1
+          }
+        })
+        
+    }, 10000)
+  },[])
 
 
   return (
--- src/components/Dial.js
+++ src/components/Dial.js
@@ -9,6 +9,10 @@ function Dial({date}) {
     <div className='dial'>
       <h3>{`${(date.hour % 12).toString().padStart(2, '0')}:${(date.min).toString().padStart(2, '0')}:${(date.sec).toString().padStart(2, '0')} ${date.hour > 12 && date.hour < 24 ? 'PM' : 'AM'}`}</h3>
       <h5>{date.date} {month[date.month]}, {days[date.day % 7]}</h5>
+  
+      {date.temp < 20 && <p style={{color: '#2F90FE'}}><i className="fa-solid fa-temperature-three-quarters"></i> {date.temp}°C</p>}
+      {date.temp >= 20 && date.temp < 32 && <p style={{color: 'orange'}}><i className="fa-solid fa-temperature-three-quarters"></i> {date.temp}°C</p>}
+      {date.temp >= 32 && <p style={{color: 'red'}}><i className="fa-solid fa-temperature-three-quarters"></i> {date.temp}°C</p>}
     </div>
   )
 }
